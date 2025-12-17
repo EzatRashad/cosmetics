99e1e739a0052ce788bc3dfac168cfce6f8d8b5c
Ezat Rashad <123140512+EzatRashad@users.noreply.github.com>
2025-12-17 15:22:49 +0200

    implement the splash screen with animations.

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,149 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppIcon
+  /// assets/images/app_icon.png
+  static const String assetsImagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for assetsImagesArrow
+  /// assets/images/arrow.svg
+  static const String assetsImagesArrow = "assets/images/arrow.svg";
+
+  /// Assets for assetsImagesArrowBack
+  /// assets/images/arrow_back.svg
+  static const String assetsImagesArrowBack = "assets/images/arrow_back.svg";
+
+  /// Assets for assetsImagesCard
+  /// assets/images/card.svg
+  static const String assetsImagesCard = "assets/images/card.svg";
+
+  /// Assets for assetsImagesCart
+  /// assets/images/cart.svg
+  static const String assetsImagesCart = "assets/images/cart.svg";
+
+  /// Assets for assetsImagesCategories
+  /// assets/images/categories.svg
+  static const String assetsImagesCategories = "assets/images/categories.svg";
+
+  /// Assets for assetsImagesCheckOutArrow
+  /// assets/images/check_out_arrow.svg
+  static const String assetsImagesCheckOutArrow = "assets/images/check_out_arrow.svg";
+
+  /// Assets for assetsImagesDialogCheck
+  /// assets/images/dialog_check.png
+  static const String assetsImagesDialogCheck = "assets/images/dialog_check.png";
+
+  /// Assets for assetsImagesDiscount
+  /// assets/images/discount.svg
+  static const String assetsImagesDiscount = "assets/images/discount.svg";
+
+  /// Assets for assetsImagesEditInfo
+  /// assets/images/edit_info.svg
+  static const String assetsImagesEditInfo = "assets/images/edit_info.svg";
+
+  /// Assets for assetsImagesHome
+  /// assets/images/home.svg
+  static const String assetsImagesHome = "assets/images/home.svg";
+
+  /// Assets for assetsImagesHomeCard
+  /// assets/images/home_card.svg
+  static const String assetsImagesHomeCard = "assets/images/home_card.svg";
+
+  /// Assets for assetsImagesHomeMakup
+  /// assets/images/home_makup.png
+  static const String assetsImagesHomeMakup = "assets/images/home_makup.png";
+
+  /// Assets for assetsImagesHomeProduct
+  /// assets/images/home_product.png
+  static const String assetsImagesHomeProduct = "assets/images/home_product.png";
+
+  /// Assets for assetsImagesHomeProduct2
+  /// assets/images/home_product2.png
+  static const String assetsImagesHomeProduct2 = "assets/images/home_product2.png";
+
+  /// Assets for assetsImagesLoginLogo
+  /// assets/images/login_logo.png
+  static const String assetsImagesLoginLogo = "assets/images/login_logo.png";
+
+  /// Assets for assetsImagesLogout
+  /// assets/images/logout.svg
+  static const String assetsImagesLogout = "assets/images/logout.svg";
+
+  /// Assets for assetsImagesMisa
+  /// assets/images/misa.svg
+  static const String assetsImagesMisa = "assets/images/misa.svg";
+
+  /// Assets for assetsImagesOffer
+  /// assets/images/offer.svg
+  static const String assetsImagesOffer = "assets/images/offer.svg";
+
+  /// Assets for assetsImagesOnBoradingOne
+  /// assets/images/on_borading_one.png
+  static const String assetsImagesOnBoradingOne = "assets/images/on_borading_one.png";
+
+  /// Assets for assetsImagesOnBoradingThree
+  /// assets/images/on_borading_three.png
+  static const String assetsImagesOnBoradingThree = "assets/images/on_borading_three.png";
+
+  /// Assets for assetsImagesOnBoradingTwo
+  /// assets/images/on_borading_two.png
+  static const String assetsImagesOnBoradingTwo = "assets/images/on_borading_two.png";
+
+  /// Assets for assetsImagesOrder
+  /// assets/images/order.svg
+  static const String assetsImagesOrder = "assets/images/order.svg";
+
+  /// Assets for assetsImagesOrderHistory
+  /// assets/images/order_history.svg
+  static const String assetsImagesOrderHistory = "assets/images/order_history.svg";
+
+  /// Assets for assetsImagesOut
+  /// assets/images/out.svg
+  static const String assetsImagesOut = "assets/images/out.svg";
+
+  /// Assets for assetsImagesProf
+  /// assets/images/prof.png
+  static const String assetsImagesProf = "assets/images/prof.png";
+
+  /// Assets for assetsImagesProfile
+  /// assets/images/profile.svg
+  static const String assetsImagesProfile = "assets/images/profile.svg";
+
+  /// Assets for assetsImagesSearch
+  /// assets/images/search.svg
+  static const String assetsImagesSearch = "assets/images/search.svg";
+
+  /// Assets for assetsImagesSettings
+  /// assets/images/settings.svg
+  static const String assetsImagesSettings = "assets/images/settings.svg";
+
+  /// Assets for assetsImagesSplashLogo
+  /// assets/images/splash_logo.png
+  static const String assetsImagesSplashLogo = "assets/images/splash_logo.png";
+
+  /// Assets for assetsImagesSplashText
+  /// assets/images/splash_text.png
+  static const String assetsImagesSplashText = "assets/images/splash_text.png";
+
+  /// Assets for assetsImagesSuccess
+  /// assets/images/success.json
+  static const String assetsImagesSuccess = "assets/images/success.json";
+
+  /// Assets for assetsImagesVisibilyOff
+  /// assets/images/visibily_off.svg
+  static const String assetsImagesVisibilyOff = "assets/images/visibily_off.svg";
+
+  /// Assets for assetsImagesVisiblily
+  /// assets/images/visiblily.svg
+  static const String assetsImagesVisiblily = "assets/images/visiblily.svg";
+
+  /// Assets for assetsImagesVoucher
+  /// assets/images/voucher.svg
+  static const String assetsImagesVoucher = "assets/images/voucher.svg";
+
+  /// Assets for assetsImagesWallet
+  /// assets/images/wallet.svg
+  static const String assetsImagesWallet = "assets/images/wallet.svg";
+}
+
---
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -0,0 +1,76 @@
+import 'package:cosmetics/core/style/app_colors.dart';
+import 'package:cosmetics/core/utils/app_images.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+
+    _fadeAnimation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.8,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 1), () {
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Image(
+                  image: AssetImage(Assets.assetsImagesSplashLogo),
+                  width: 150,
+                  height: 150,
+                ),
+                SizedBox(height: 16),
+                Image(
+                  image: AssetImage(Assets.assetsImagesSplashText),
+                  width: 100,
+                  height: 46,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
