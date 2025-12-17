ddd3008b90e2af851689c56155a444e4b1aed8fe
Ezat Rashad <123140512+EzatRashad@users.noreply.github.com>
2025-12-17 14:26:43 +0200

    init project and set app icon

---
+++ lib/core/style/text_styles.dart
@@ -0,0 +1,43 @@
+import 'package:cosmetics/core/style/app_colors.dart' ;
+import 'package:flutter/cupertino.dart';
+
+abstract class Styles {
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w900,
+    letterSpacing: 1.2,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle8 = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+  static const textStyle10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    color: AppColors.background,
+  );
+}
+
+// const kGtSectraFine = 'GT Sectra Fine';
